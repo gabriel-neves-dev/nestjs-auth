b9513d477a8c3b9570056f41918f68c15f1432bc
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-16 12:54:43 -0300

    crioado class createUserDto para criacao de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { Roles } from '@prisma/client';
+
+export class CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+}
+// dto -> data transfer object
+// sõ tem dados, sem lõgica, possivel transferir entre camadas do projeto
