6cc9932e1a6edb06c6ed216cfe5c0899588bcb87
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-16 12:55:43 -0300

    Update de imports para contemplar modulos do prisma e de authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   //registro de artefatos do modulo
-  imports: [],
+  imports: [UsersModule, PrismaModule, AuthModule],
 
   controllers: [AppController],
-  
+
   providers: [AppService],
 })
 export class AppModule {}
-
 
 // tudo no nest fica no modulo
 // modulo é uma classe que tem decorator @Module do javascript es7
@@ -20,6 +22,5 @@ export class AppModule {}
 // controllers: lista de controllers que serão usados nesse modulo
 // providers: lista de providers que serão usados nesse modulo
 // exports: lista de providers que serão exportados para outros modulos
-
 
 // só funcionara se estiver registrado no modulo
