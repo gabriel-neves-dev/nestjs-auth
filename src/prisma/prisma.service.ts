dba611a0e05c97899176ff77382ff707030d1096
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-11-25 23:44:40 -0300

    aded pool and adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,8 @@
+// filepath: /Users/gabrielneves/Documents/Projetos/nestjs-auth/src/prisma/prisma.service.ts
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg'; // Instale com npm install pg
 
 // serve para nest se comunicar com prisma
 // prisma client é a instancia que fara todas as operacoes
@@ -8,6 +11,19 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    // Crie um pool de conexão PostgreSQL
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+
+    // Use o adapter PrismaPg para PostgreSQL
+    const adapter = new PrismaPg(pool);
+
+    // Passe o adapter ao PrismaClient
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    super({ adapter } );
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
